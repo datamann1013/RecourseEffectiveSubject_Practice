28df71f959f002718e25f56fb9e022651d9234d4
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-05-02 12:37:17 +0200

    SelectionSort.py passing tests. empty returns empty, one element returns right away, array is not sorted in original, but in a copy, the elements at the beginning is not run through more after their turn.

--- algorithms/SelectionSort/SelectionSort.py
+++ algorithms/SelectionSort/SelectionSort.py
@@ -1,4 +1,12 @@
 def selection_sort(array):
     if len(array) == 0:
         return []
-    return
+    if len(array) == 1:
+        return array
+    sorted_array = array.copy()
+
+    for i in range(len(sorted_array)):
+        for j in range(i+1, len(sorted_array)):
+            if sorted_array[i] > sorted_array[j]:
+                sorted_array[i], sorted_array[j] = sorted_array[j], sorted_array[i]
+    return sorted_array
