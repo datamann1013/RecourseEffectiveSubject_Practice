7fdc8d543e81283493b209d62ab14d381970f5f8
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-05-03 22:01:47 +0200

    Comprehensionalised bucket_sort.py

--- Algorithms/BucketSort/bucket_sort.py
+++ Algorithms/BucketSort/bucket_sort.py
@@ -1,18 +1,11 @@
 def bucket_sort(array, k=10):
-    if len(array) == 1 or len(array) == 0:
-        return array
+    if len(array) == 1 or len(array) == 0: return array
 
     copy_array = array.copy()
     min_val, max_val = min(copy_array), max(copy_array)
     range_val = (max_val - min_val) or 1
-    buckets = [[] for _ in range(k)]
 
-    for x in array:
-        idx = (x - min_val) * (k - 1) // range_val
-        buckets[idx].append(x)
-
-    sorted_array = []
-    for bucket in buckets:
-        sorted_array.extend(sorted(bucket))
+    buckets = [[x for x in array if (x - min_val) * (k - 1) // range_val == idx] for idx in range(k)]
+    sorted_array = [item for bucket in buckets for item in sorted(bucket)]
 
     return sorted_array
