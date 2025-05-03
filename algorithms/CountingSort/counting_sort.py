c02c6a1fa23e548eb58a2d548d5b23c978499d15
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-05-03 09:45:46 +0200

    Implemented counting_sort.py. Tests pass

---
+++ algorithms/CountingSort/counting_sort.py
@@ -0,0 +1,26 @@
+def counting_sort(array):
+    if len(array) == 1 or len(array) == 0:
+        return array
+
+    sorted_array = array.copy()
+    min_val = min(sorted_array)
+    max_val = max(sorted_array)
+    length = max_val - min_val + 1
+
+    number_array = [0] * length
+
+    for j in sorted_array:
+        number_array[j - min_val] +=1
+
+    for i in range(1, length):
+        number_array[i] += number_array[i - 1]
+
+    n = len(sorted_array)
+    output = [None] * n
+
+    for x in reversed(sorted_array):
+        idx = x - min_val
+        number_array[idx] -= 1
+        output[number_array[idx]] = x
+
+    return output
