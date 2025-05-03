438a762d27d406de6af8b74fa31454e9b3c27fff
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-05-03 21:22:33 +0200

    Implemented comperehension style in bubble_sort.py (comprehension si not ment to do multuiple operations, so its not more compact.

--- Algorithms/BubbleSort/bubble_sort.py
+++ Algorithms/BubbleSort/bubble_sort.py
@@ -1,18 +1,18 @@
 def bubble_sort(array):
-    if len(array) == 1 or len(array) == 0:
-        return array
+    if len(array) == 1 or len(array) == 0: return array
 
     sorted_array = array.copy()
     length = len(sorted_array)
 
     while True:
         swapped = False
-        for i in range(1, length):
-            if sorted_array[i - 1] > sorted_array[i]:
-                sorted_array[i - 1], sorted_array[i] = sorted_array[i], sorted_array[i - 1]
-                swapped = True
-        if not swapped:
+
+        swap_pairs = [(i - 1, i) for i in range(1, length)
+                      if sorted_array[i - 1] > sorted_array[i]]
+
+        for a, b in swap_pairs:
+            sorted_array[a], sorted_array[b] = sorted_array[b], sorted_array[a]
+        if not swap_pairs:
             break
-        length -= 1
 
     return sorted_array
